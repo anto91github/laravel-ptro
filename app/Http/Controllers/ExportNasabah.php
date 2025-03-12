c6d6bf3dac9f90d75bad19413b3d2cab91508a0a
BCASekuritas <it@bcasekuritas.co.id>
2025-03-12 10:59:49 +0700

    test connection to postgreSQL

--- app/Http/Controllers/ExportNasabah.php
+++ app/Http/Controllers/ExportNasabah.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BcasAkun;
 
 class ExportNasabah extends Controller
 {
     public function index(Request $request)
     {
+        $bcas_akun = BcasAkun::all();
+        // dd($bcas_akun);
         return view('ExportNasabah/index');
     }
 }
