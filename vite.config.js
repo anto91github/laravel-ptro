0cb77fb03b50e1d640ab2ff28c70b79b7b0eff2f
BCASekuritas <it@bcasekuritas.co.id>
2025-03-12 10:05:37 +0700

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import { resolve } from 'path';
+
+export default defineConfig({
+    server: {
+        port: 5173,
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '~coreui': path.resolve(__dirname, 'node_modules/@coreui/coreui'),
+        }
+    },
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+    }
+});
